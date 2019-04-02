1d78c49253040b1b868b7c7cec3a7445db7de5de
BigMurry <hhdiao@siom.ac.cn>
2019-04-02 15:50:39 +0800

    feat: add support for campaign

--- src/analytics/prod.js
+++ src/analytics/prod.js
@@ -2,6 +2,14 @@ import ReactGA from "react-ga";
 
 const IS_BROWSER = typeof window !== "undefined";
 
+const QS_KEY = {
+  'utm_source': 'campaignSource',
+  'utm_medium': 'campaignMedium',
+  'utm_campaign': 'campaignName',
+  'utm_term': 'campaignKeyword',
+  'utm_content': 'campaignContent'
+};
+
 export function init(code) {
   if (IS_BROWSER && !window.GA_INITIALIZED && code) {
     ReactGA.initialize(code);
@@ -9,13 +17,26 @@ export function init(code) {
 }
 
 export function pageview() {
+  window.location.search
+    .replace(/^\?/, '')
+    .split('&')
+    .map(item => {
+      const [key, value] = item.split('=');
+      if (QS_KEY.hasOwnProperty(key)) {
+        const newKey = QS_KEY[key];
+        ReactGA.set({[newKey]: value});
+        try {
+          window.sessionStorage.setItem(newKey, value);
+        } catch (e) {}
+      }
+    });
   ReactGA.set({ page: window.location.pathname });
   ReactGA.pageview(window.location.pathname);
 }
 
-export function event(category = "", action = "") {
+export function event(category = "", action = "", label = '', value = '') {
   if (category && action) {
-    ReactGA.event({ category, action });
+    ReactGA.event({ category, action, label, value });
   }
 }
 
